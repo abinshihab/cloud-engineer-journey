05910860d91ddd0c4c910f45cbe18a2c518de1bc
abinshihab <22618390+abinshihab@users.noreply.github.com>
2025-07-12 14:07:26 +0400

    Convert your current VPC setup into a clean, reusable Terraform module

--- Week1/terraform-vpc-module/main.tf
+++ Week1/terraform-vpc-module/main.tf
@@ -7,44 +7,54 @@ resource "aws_vpc" "main" {
   cidr_block           = var.vpc_cidr
   enable_dns_support   = true
   enable_dns_hostnames = true
-  tags = {
+
+  tags = merge(var.tags, {
     Name = "${var.project}-vpc"
-  }
+  })
 }
 
 resource "aws_internet_gateway" "igw" {
   vpc_id = aws_vpc.main.id
-  tags = {
+
+  tags = merge(var.tags, {
     Name = "${var.project}-igw"
-  }
+  })
 }
+
 
 resource "aws_subnet" "public" {
   count                   = length(var.public_subnet_cidrs)
   vpc_id                  = aws_vpc.main.id
   cidr_block              = var.public_subnet_cidrs[count.index]
-  map_public_ip_on_launch = true
   availability_zone       = element(var.azs, count.index)
-  tags = {
+  map_public_ip_on_launch = true
+
+  tags = merge(var.tags, {
     Name = "${var.project}-public-${count.index + 1}"
-  }
+  })
 }
 
 resource "aws_route_table" "public" {
   vpc_id = aws_vpc.main.id
-  tags = {
-    Name = "${var.project}-public-rt"
-  }
-}
 
-resource "aws_route" "public_internet_access" {
-  route_table_id         = aws_route_table.public.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.igw.id
+  tags = merge(var.tags, {
+    Name = "${var.project}-public-rt"
+  })
 }
 
 resource "aws_route_table_association" "public" {
   count          = length(var.public_subnet_cidrs)
   subnet_id      = aws_subnet.public[count.index].id
   route_table_id = aws_route_table.public.id
+}
+
+resource "aws_subnet" "private" {
+  count             = length(var.private_subnet_cidrs)
+  vpc_id            = aws_vpc.main.id
+  cidr_block        = var.private_subnet_cidrs[count.index]
+  availability_zone = element(var.azs, count.index)
+
+  tags = merge(var.tags, {
+    Name = "${var.project}-private-${count.index + 1}"
+  })
 }
--- Week1/terraform-vpc-module/variables.tf
+++ Week1/terraform-vpc-module/variables.tf
@@ -1,12 +1,11 @@
-
 variable "region" {
-  description = "AWS region to deploy into"
+  description = "AWS region to deploy resources"
   type        = string
   default     = "us-east-1"
 }
 
 variable "project" {
-  description = "Project name prefix"
+  description = "Project name prefix for tagging"
   type        = string
   default     = "devops-lab"
 }
@@ -17,14 +16,29 @@ variable "vpc_cidr" {
   default     = "10.0.0.0/16"
 }
 
+variable "azs" {
+  description = "List of Availability Zones to deploy subnets"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
+
 variable "public_subnet_cidrs" {
   description = "List of public subnet CIDR blocks"
   type        = list(string)
   default     = ["10.0.1.0/24", "10.0.2.0/24"]
 }
 
-variable "azs" {
-  description = "Availability zones"
+variable "private_subnet_cidrs" {
+  description = "List of private subnet CIDR blocks"
   type        = list(string)
-  default     = ["us-east-1a", "us-east-1b"]
+  default     = ["10.0.101.0/24", "10.0.102.0/24"]
+}
+
+variable "tags" {
+  description = "Common tags applied to all resources"
+  type        = map(string)
+  default = {
+    Environment = "dev"
+    Owner       = "Ahmed"
+  }
 }
