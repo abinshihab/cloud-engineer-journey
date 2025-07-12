9d8cf831d38a83dc9f809468d87717d731a2a378
abinshihab <abushihab_330@hotmail.com>
2025-07-12 09:49:49 +0400

    Fix: removed submodule reference and added as a regular folder

---
+++ Desktop/8-Week-Challenge/Week1/terraform-vpc-module/variables.tf
@@ -0,0 +1,30 @@
+
+variable "region" {
+  description = "AWS region to deploy into"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "project" {
+  description = "Project name prefix"
+  type        = string
+  default     = "devops-lab"
+}
+
+variable "vpc_cidr" {
+  description = "CIDR block for the VPC"
+  type        = string
+  default     = "10.0.0.0/16"
+}
+
+variable "public_subnet_cidrs" {
+  description = "List of public subnet CIDR blocks"
+  type        = list(string)
+  default     = ["10.0.1.0/24", "10.0.2.0/24"]
+}
+
+variable "azs" {
+  description = "Availability zones"
+  type        = list(string)
+  default     = ["us-east-1a", "us-east-1b"]
+}
