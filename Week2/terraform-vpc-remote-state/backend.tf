28c23a0131edb0672c38e68f21b0f03a737423ee
abinshihab <abushihab_330@hotmail.com>
2025-07-16 21:25:00 +0400

    Update backend file

--- Week2/terraform-vpc-remote-state/backend.tf
+++ Week2/terraform-vpc-remote-state/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "Devops-s3-backend-bucket"
+    bucket         = "Dev-s3-backend-bucket"
     key            = "terraform/state/week2/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
