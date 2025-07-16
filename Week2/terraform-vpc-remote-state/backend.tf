3294cbbf30b996953d308e4baee1d0fb438082b6
abinshihab <abushihab_330@hotmail.com>
2025-07-16 22:25:25 +0400

    Configure remote backend for VPC module

--- Week2/terraform-vpc-remote-state/backend.tf
+++ Week2/terraform-vpc-remote-state/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket         = "Dev-s3-backend-bucket"
-    key            = "terraform/state/week2/terraform.tfstate"
+    bucket         = "dev-s3-backend-bucket"
+    key            = "vpc/dev/terraform.tfstate"
     region         = "us-east-1"
     dynamodb_table = "terraform-locks"
     encrypt        = true
