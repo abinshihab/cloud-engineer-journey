4b4bb87f398a9049aa996789b4a105ae55a42ef2
abinshihab <abushihab_330@hotmail.com>
2025-07-25 01:24:12 +0400

    .

---
+++ Week1/terraform-vpc-module/examples/basic/main.tf
@@ -0,0 +1,15 @@
+module "vpc" {
+  source = "../../"
+
+  vpc_cidr     = "10.0.0.0/16"
+  environment  = "dev"
+  azs          = ["us-east-1a", "us-east-1b"]
+  public_subnet_cidrs  = ["10.0.1.0/24", "10.0.2.0/24"]
+  private_subnet_cidrs = ["10.0.3.0/24", "10.0.4.0/24"]
+
+  tags = {
+    owner      = "ahmed"
+    managed_by = "terraform"
+  }
+}
+
