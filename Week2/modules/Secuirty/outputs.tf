2e2b2d1f95a8b4b4da4a2f470ac418fc863caeac
abinshihab <abushihab_330@hotmail.com>
2025-07-29 23:31:41 +0400

    Update for day15

--- Week2/modules/Secuirty/outputs.tf
+++ Week2/modules/Secuirty/outputs.tf
@@ -1,3 +1,7 @@
 output "web_sg_id" {
   value = aws_security_group.web_sg.id
 }
+output "alb_sg_id" {
+  description = "Security group ID for ALB or web instances"
+  value       = aws_security_group.web_sg.id
+}
