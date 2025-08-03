0ab478c66a914d48df67afe5fb88dcc8013dc177
abinshihab <abushihab_330@hotmail.com>
2025-08-03 08:10:47 +0400

    Update Day 16- Week3

--- Week3/modules/vpc/outputs.tf
+++ Week3/modules/vpc/outputs.tf
@@ -13,3 +13,7 @@ output "private_subnet_ids" {
 output "igw_id" {
   value = aws_internet_gateway.igw.id
 }
+output "security_group_ids" {
+  description = "List of security group IDs created by VPC module"
+  value       = [aws_security_group.vpc_default_sg.id]
+}
